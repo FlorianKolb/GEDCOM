074a66a5ec7a81376a78543824f50e9bdafbab38
Florian Kolb <kolb@noxum.com>
2015-08-30 14:20:02 +0200

    added GedcomToXml command line tool

---
+++ GedcomToXml/Program.cs
@@ -0,0 +1,29 @@
+﻿using CommandLine;
+using GedcomLibrary;
+using System;
+using System.Xml.Linq;
+
+namespace GedcomToXml
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.Title = "GedcomToXml";
+
+      Options commandLineOptions = new Options();
+      CommandLine.Parser.Default.ParseArguments(args, commandLineOptions);
+
+      XDocument document = GedcomReader.ToXml(commandLineOptions.GedcomFile);
+      document.Save(commandLineOptions.OutputFile);
+    }
+  }
+
+  public class Options
+  {
+    [Option('i', "input", Required = true, HelpText = "Input GEDCOM file")]
+    public string GedcomFile { get; set; }
+    [Option('o', "output", Required = true, HelpText = "Output XML file")]
+    public string OutputFile { get; set; }
+  }
+}
